7d2140870b85e57a17cb39310e0fd4f440366bec
Pbjsd <104098660+Pbjsd@users.noreply.github.com>
2023-04-30 22:36:41 -0400

    Initial Commit

---
+++ Bullseye/App/BullseyeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BullseyeApp.swift
+//  Bullseye
+//
+//  Created by Panchi on 4/5/23.
+//
+
+import SwiftUI
+
+@main
+struct BullseyeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
